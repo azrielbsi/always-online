1a69757cac4ff4f8e85a0d58eaa159fa62291e39
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2024-02-16 05:03:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ bot.on("error", (err) => {
 // Function to update the bot's status
 function updateStatus() {
     const status = statuses[currentStatusIndex];
-    bot.editStatus("idle", status);
+    bot.editStatus("online", status);
     currentStatusIndex = (currentStatusIndex + 1) % statuses.length; // Move to the next status in the array
 }
 
