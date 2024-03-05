b4a06bae194af7a028a0f770c7abef99e19ec75b
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2024-03-05 22:29:32 +0700

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -3,9 +3,4 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('schoolprograms.my.id');
-});
-
-const PORT = 8080;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+}).listen(8080);
